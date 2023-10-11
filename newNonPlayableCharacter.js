4548430f2c3e1d7a6d0eb5ad965988d774a1e289
rxnvaz18 <54378927+rxnvaz18@users.noreply.github.com>
2023-10-10 18:42:28 -0700

    changed walkEast into Asynch Await promise

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,9 +23,11 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(time) 
+        stop()
     }
 
     function walkNorth() {
